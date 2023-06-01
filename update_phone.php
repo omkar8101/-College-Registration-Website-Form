0d09a3d53ba50fe4fcf776c733b29830f552c348
Omkar Shinde <89976480+omkar8101@users.noreply.github.com>
2023-06-02 00:11:18 +0530

    Add files via upload

---
+++ update_phone.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "5CG9266KP8";
+$username = "sa";
+$password = "C123456789";
+$dbname = "Student_Information";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$newPhone = $_POST['newPhone'];
+
+$phone = ""; // Replace with the current student's phone number
+$sql = "UPDATE students SET phone = '$newPhone' WHERE phone = '$phone'";
+$result = $conn->query($sql);
+
+if ($result === false) {
+    die("Error executing query: " . $conn->error);
+} else {
+    echo "Phone number updated successfully!";
+}
+
+$conn->close();
+?>
