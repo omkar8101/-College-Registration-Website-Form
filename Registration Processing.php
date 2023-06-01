1332f4a6908ed2e8567e316da45d2f994cc1296d
Omkar Shinde <89976480+omkar8101@users.noreply.github.com>
2023-06-02 00:09:37 +0530

    Add files via upload

---
+++ Registration Processing.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "5CG9266KP8";
+$username = "sa";
+$password = "C123456789";
+$dbname = "Student_Information";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$password = $_POST['password'];
+$subject1 = $_POST['subject1'];
+$subject2 = $_POST['subject2'];
+$subject3 = $_POST['subject3'];
+$subject4 = $_POST['subject4'];
+$subject5 = $_POST['subject5'];
+
+
+$sql = "INSERT INTO students (name, phone, password, subject1, subject2, subject3, subject4, subject5)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssssss", $name, $phone, $password, $subject1, $subject2, $subject3, $subject4, $subject5);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo "Registration successful!";
+} else {
+    echo "Error occurred while registering the student.";
+}
+
+
+$stmt->close();
+$conn->close();
+?>
